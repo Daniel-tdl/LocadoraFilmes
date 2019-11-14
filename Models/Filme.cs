f2ba001155664d1d1cc5ff4e398c0b353e746140
Daniel_jr <daniel@extradigital.com.br>
2019-11-14 16:41:31 -0300

    Criação das classes do modelo

---
+++ Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraFilmes.Models
+{
+    public class Filme
+    {
+        public int FilmeID { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public Char Ativo { get; set; }
+        public int GeneroID { get; set; }
+
+        [Foreignkey("GeneroID")]
+        public virtual Genero Genero { get; set; }
+    }
+}
